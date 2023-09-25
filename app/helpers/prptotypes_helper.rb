d76a1a1f4060f8894e3a2166014fafc83f10f1cb
ri0629 <tr08792@gmail.com>
2023-09-26 08:31:09 +0900

    first commit

---
+++ app/helpers/prptotypes_helper.rb
@@ -0,0 +1,2 @@
+module PrptotypesHelper
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "prototypes#index"
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: :create
+  end
+
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+ resources :users, only: :show
+end
